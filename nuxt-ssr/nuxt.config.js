6755f2a2a9dc70cae82e073b30000daebf203529
Masahiro Takehara <azukibar21@gmail.com>
2019-10-22 21:19:12 +0900

    feat : APIの情報を表示できた

--- nuxt-ssr/nuxt.config.js
+++ nuxt-ssr/nuxt.config.js
@@ -40,7 +40,10 @@ export default {
   modules: [
     // Doc: https://bootstrap-vue.js.org
     'bootstrap-vue/nuxt',
+    '@nuxtjs/axios',
   ],
+  axios: {
+  },
   /*
   ** Build configuration
   */
---
+++ nuxt-ssr/pages/hello.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    {{ data }}
+  </div>
+</template>
+
+
+<script>
+  export default {
+    async asyncData ({ app }) {
+      const data = await app.$axios.$get(`http://127.0.0.1:8000/api`)
+      return { data }
+    }
+  }
+</script>
